5c3e34363b9e040c63e25ab1dad59e2ad64f6909
p <pberben@gmx.de>
2023-05-31 14:00:42 +0200

    download latens scaler

--- cog-multi/predict.py
+++ cog-multi/predict.py
@@ -53,6 +53,14 @@ class Predictor(BasePredictor):
             local_files_only=True,
         ).to("cuda")
 
+        start = time.time()
+        print("Loading latent upscaler...")
+        self.latent_upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained(
+            "stabilityai/sd-x2-latent-upscaler", 
+            torch_dtype=torch.float16,
+            local_files_only=False,
+        ).to("cuda")
+        print("loading latent upscaler took: %0.2f" % (time.time() - start))
 
 
         self.weights_download_cache = WeightsDownloadCache()
@@ -278,15 +286,6 @@ class Predictor(BasePredictor):
         pipe = self.get_weights(weights)
         print("loading weights took: %0.2f" % (time.time() - start))
 
-        start = time.time()
-        print("Loading latent upscaler...")
-        self.latent_upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained(
-            "stabilityai/sd-x2-latent-upscaler", 
-            cache_dir=settings.MODEL_CACHE,
-            torch_dtype=torch.float16,
-            local_files_only=True,
-        ).to("cuda")
-        print("loading latent upscaler took: %0.2f" % (time.time() - start))
 
         start = time.time()
         if image:
