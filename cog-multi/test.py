c3ad7e0974f16a48e65429f1897592b045085cb7
p <pberben@gmx.de>
2023-06-12 10:16:21 +0200

    tongue pt

--- cog-multi/test.py
+++ cog-multi/test.py
@@ -4,9 +4,9 @@ import torch
 model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-pipe.load_textual_inversion("sd-concepts-library/cat-toy")
+pipe.load_textual_inversion("./awaitingtongue.pt")
 
 prompt = "A <cat-toy> backpack"
 
-image = pipe(prompt, num_inference_steps=50).images[0]
+image = pipe(prompt, num_inference_steps=30).images[0]
 image.save("cat-backpack.png")
