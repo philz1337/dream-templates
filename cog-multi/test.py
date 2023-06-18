dca69b6c68cbe7ce014cdaeca00f9b8f0c5da48c
p <pberben@gmx.de>
2023-06-18 21:07:09 +0200

    tiles

--- cog-multi/test.py
+++ cog-multi/test.py
@@ -1,24 +1,42 @@
-from diffusers import StableDiffusionPipeline
+
 import torch
+from PIL import Image
+from diffusers import ControlNetModel, DiffusionPipeline, DDIMScheduler
+from diffusers.utils import load_image
 
-model_id = "philz1337/rv2.0"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+def resize_for_condition_image(input_image: Image, resolution: int):
+    input_image = input_image.convert("RGB")
+    H, W = input_image.size
+    k = float(resolution) / min(H, W)
+    H *= k
+    W *= k
+    H = int(round(H / 64.0)) * 64
+    W = int(round(W / 64.0)) * 64
+    img = input_image.resize((W, H), resample=Image.LANCZOS if k > 1 else Image.AREA)
+    return img
 
+controlnet = ControlNetModel.from_pretrained('takuma104/control_v11', 
+                                             subfolder='control_v11f1e_sd15_tile',
+                                             torch_dtype=torch.float16)
+pipe = DiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    custom_pipeline="stable_diffusion_controlnet_img2img",
+    controlnet=controlnet,
+    torch_dtype=torch.float16).to('cuda')
+pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+pipe.enable_xformers_memory_efficient_attention()
 
-pipe.load_textual_inversion("./ti/negative_hand-neg.pt", token="<ti-neghand>")
-pipe.load_textual_inversion("./ti/badhandv4.pt", token="<badhandv4>")
-pipe.load_textual_inversion("./ti/easynegative.pt", token="<easynegative>")
-pipe.load_textual_inversion("./ti/ng_deepnegative_v1_75t.pt", token="<ng_deepnegative>")
-pipe.load_textual_inversion("./ti/pureerosface_v1.pt", token="<pureerosface>")
+source_image = load_image('https://github.com/lllyasviel/ControlNet-v1-1-nightly/raw/main/test_imgs/dog64.png')
 
-pipe.load_textual_inversion("./ti/angry512.pt", token="<em-angry>")
-pipe.load_textual_inversion("./ti/happy512.pt", token="<em-happy>")
-pipe.load_textual_inversion("./ti/shock512.pt", token="<em-shock>")
-pipe.load_textual_inversion("./ti/smile512.pt", token="<em-smile>")
-
-prompt = "woman with beautiful face, close up, beauty marks, sandy skin, photo, detailed,(moody lighting), (sharp focus), "
-negative_prompt= "<ti-neghand>"
-
-generator = torch.Generator(device="cuda").manual_seed(3336)
-image = pipe(prompt, generator=generator, negative_prompt=negative_prompt, num_inference_steps=50).images[0]
-image.save("cat-backpack.png")
+condition_image = resize_for_condition_image(source_image, 1024)
+image = pipe(prompt="best quality", 
+     negative_prompt="blur, lowres, bad anatomy, bad hands, cropped, worst quality", 
+     image=condition_image, 
+     controlnet_conditioning_image=condition_image, 
+     width=condition_image.size[0],
+     height=condition_image.size[1],
+     strength=1.0,
+     generator=torch.manual_seed(0),
+     num_inference_steps=32,
+     ).images[0]
+image.save("tiles.png")
