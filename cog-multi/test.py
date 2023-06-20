7beefcc6a199b24cf17436c7dd8610bea5c0a6d0
p <pberben@gmx.de>
2023-06-20 16:54:26 +0200

    x

--- cog-multi/test.py
+++ cog-multi/test.py
@@ -1,32 +1,21 @@
 from diffusers import DiffusionPipeline
-from transformers import CLIPImageProcessor, CLIPModel
 import torch
-
+from diffusers.utils import load_image
 
 guided_pipeline = DiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
     custom_pipeline="stable_diffusion_reference",
-    torch_dtype=torch.float16,
+    safety_checker=None,
+    torch_dtype=torch.float16
 )
 guided_pipeline.enable_attention_slicing()
 guided_pipeline = guided_pipeline.to("cuda")
 
-prompt = "fantasy book cover, full moon, fantasy forest landscape, golden vector elements, fantasy magic, dark light night, intricate, elegant, sharp focus, illustration, highly detailed, digital painting, concept art, matte, art by WLOP and Artgerm and Albert Bierstadt, masterpiece"
+input_image = load_image("https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png")
 
-generator = torch.Generator(device="cuda").manual_seed(0)
-images = []
-for i in range(4):
-    image = guided_pipeline(
-        prompt,
-        num_inference_steps=50,
-        guidance_scale=7.5,
-        clip_guidance_scale=100,
-        num_cutouts=4,
-        use_cutouts=False,
-        generator=generator,
-    ).images[0]
-    images.append(image)
-    
-# save images locally
-for i, img in enumerate(images):
-    img.save(f"./clip_guided_sd/image_{i}.png")
+
+result_img = guided_pipeline(ref_image=input_image,
+      prompt="1girl",
+      num_inference_steps=20,
+      reference_attn=True,
+      reference_adain=True).images[0]
