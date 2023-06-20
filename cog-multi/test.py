2d722faf52cc1b36c0bbb1a1cf10e77e0622c00a
p <pberben@gmx.de>
2023-06-20 16:48:46 +0200

    x

--- cog-multi/test.py
+++ cog-multi/test.py
@@ -1,20 +1,39 @@
+from diffusers import DiffusionPipeline
+from transformers import CLIPImageProcessor, CLIPModel
 import torch
-from diffusers import (UniPCMultistepScheduler, DiffusionPipeline)
-from diffusers.utils import load_image
 
-input_image = load_image("https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png")
 
-pipe = DiffusionPipeline.from_pretrained(
-       "philz1337/clarity",
-       custom_pipeline="stable_diffusion_reference.py",
-       safety_checker=None,
-       torch_dtype=torch.float16
-       ).to('cuda:0')
+feature_extractor = CLIPImageProcessor.from_pretrained("laion/CLIP-ViT-B-32-laion2B-s34B-b79K")
+clip_model = CLIPModel.from_pretrained("laion/CLIP-ViT-B-32-laion2B-s34B-b79K", torch_dtype=torch.float16)
 
-pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 
-result_img = pipe(ref_image=input_image,
-      prompt="1girl",
-      num_inference_steps=20,
-      reference_attn=True,
-      reference_adain=True).images[0]
+guided_pipeline = DiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    custom_pipeline="clip_guided_stable_diffusion",
+    clip_model=clip_model,
+    feature_extractor=feature_extractor,
+    
+    torch_dtype=torch.float16,
+)
+guided_pipeline.enable_attention_slicing()
+guided_pipeline = guided_pipeline.to("cuda")
+
+prompt = "fantasy book cover, full moon, fantasy forest landscape, golden vector elements, fantasy magic, dark light night, intricate, elegant, sharp focus, illustration, highly detailed, digital painting, concept art, matte, art by WLOP and Artgerm and Albert Bierstadt, masterpiece"
+
+generator = torch.Generator(device="cuda").manual_seed(0)
+images = []
+for i in range(4):
+    image = guided_pipeline(
+        prompt,
+        num_inference_steps=50,
+        guidance_scale=7.5,
+        clip_guidance_scale=100,
+        num_cutouts=4,
+        use_cutouts=False,
+        generator=generator,
+    ).images[0]
+    images.append(image)
+    
+# save images locally
+for i, img in enumerate(images):
+    img.save(f"./clip_guided_sd/image_{i}.png")
