d8aaa6591a0b49bd462f05f199323df293888f8f
p <pberben@gmx.de>
2023-06-27 12:57:00 +0200

    x

--- cog-multi/test.py
+++ cog-multi/test.py
@@ -1,21 +1,14 @@
 import PIL
-import requests
 import torch
-from io import BytesIO
-
 from diffusers import StableDiffusionInpaintPipeline
 
+print("torch version:   " , torch.__version__)
 
-def download_image(url):
-    response = requests.get(url)
-    return PIL.Image.open(BytesIO(response.content)).convert("RGB")
+img_path = "image.jpeg"
+mask_path = "mask.png"
 
-
-img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
-mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
-
-init_image = download_image(img_url).resize((512, 512))
-mask_image = download_image(mask_url).resize((512, 512))
+init_image = PIL.Image.open(img_path).convert("RGB")
+mask_image = PIL.Image.open(mask_path).convert("RGB")
 
 pipe = StableDiffusionInpaintPipeline.from_pretrained(
     "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16
@@ -23,7 +16,9 @@ pipe = StableDiffusionInpaintPipeline.from_pretrained(
 pipe = pipe.to("cuda")
 
 prompt = "Face of a yellow cat, high resolution, sitting on a park bench"
-image = pipe(prompt=prompt, image=init_image, mask_image=mask_image).images[0]
+image = pipe(prompt=prompt, image=init_image, mask_image=mask_image,
+             width=512, height=768
+             ).images[0]
 
-output_path = f"test.png"
+output_path = "output.png"
 image.save(output_path)
