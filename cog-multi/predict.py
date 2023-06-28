c936bee0742852632127a63ccbfdb49b53d45f8a
p <pberben@gmx.de>
2023-06-28 13:05:55 +0200

    x

--- cog-multi/predict.py
+++ cog-multi/predict.py
@@ -575,7 +575,8 @@ class Predictor(BasePredictor):
             #         "Zoom out pipeline only supports 1.5 aspect ratio. Because it is using a predefined mask."
             #     )
             pipe = self.get_pipeline(pipe, "zoom_out")
-            mask = self.load_image("/src/mask.png")
+            
+            # mask = self.load_image("/src/mask.png")
             image = self.resize_and_center_image(image)
             extra_kwargs = {
                 "prompt": prompt,
